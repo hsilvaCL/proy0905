879c7d58fc35f11e5e35ad06e1d906ea663ce841
HSO <>
2017-05-09 10:54:46 -0300

    Clase Producto

---
+++ lib/Producto.php
@@ -0,0 +1,17 @@
+<?php
+
+class Producto{
+    var $nombre="";
+    var $precio=0;
+    var $codigo="";
+    
+    /*Constructor de la clase*/
+    
+    public function __construct($nombre="", $precio=0, $codigo="") {
+        $this->nombre= $nombre;
+        $this->precio= $precio;
+        $this->codigo= $codigo;
+    }
+    
+    
+}
